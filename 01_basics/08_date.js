61ed061412f8fb6d0bb7bde1c0194f254ab35117
Aayush <ayushms1027@gmail.com>
2024-12-07 15:16:41 +0530

    date-time basics

---
+++ 01_basics/08_date.js
@@ -0,0 +1,11 @@
+let myDate = new Date();
+console.log(myDate);
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+
+let myCreatedDate = new Date(2023, 0, 30, 19, 7);
+console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toLocaleDateString());
