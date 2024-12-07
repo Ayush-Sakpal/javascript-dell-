25698fd908fb0125c1209c4ad3bbb714a969571b
Aayush <ayushms1027@gmail.com>
2024-12-07 14:51:45 +0530

    basic maths

--- 01_basics/07_nums_and_maths.js
+++ 01_basics/07_nums_and_maths.js
@@ -16,3 +16,22 @@ console.log(another_num.toPrecision(5));
 const hundreds = 1000000;
 console.log(hundreds.toLocaleString('en-US'));
 console.log(hundreds.toLocaleString('en-IN'));
+
+// ################################################## Maths ##################################################
+
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+// General formula to get random numbers between two given nos. min and max.
+const min = 10;
+const max = 30;
+
+let num = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(num);
